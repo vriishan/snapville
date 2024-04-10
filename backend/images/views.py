3ac7a8b63c0d71f69ce383b4f2ac63b062885521
Vrushab Shandilya <cooperalan97@gmail.com>
2024-04-10 00:55:29 -0700

    remove unused import

--- backend/images/views.py
+++ backend/images/views.py
@@ -2,7 +2,7 @@ from collections import defaultdict
 from .models import *
 from .serializers import *
 from rest_framework.permissions import AllowAny
-from rest_framework import viewsets, mixins
+from rest_framework import viewsets
 from images.models import Image
 from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveAPIView, DestroyAPIView, UpdateAPIView
 from rest_framework import status
